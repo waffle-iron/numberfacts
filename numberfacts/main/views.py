c4b169584cc286743faa093fc9a1f57579fc0858
ehne <ehnevault@gmail.com>
2017-05-30 21:06:45 +1000

    setup urls.py and basic homepage

--- numberfacts/main/views.py
+++ numberfacts/main/views.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def homepage(request):
+    return HttpResponse("hello nerd")
